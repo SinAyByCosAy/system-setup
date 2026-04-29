009370ddd8c950c48e003d4b2c4c3560d3c7b52b
SinAyByCosAy <tanaysriofficial@gmail.com>
2026-04-29 12:23:50 +0530

    merge install & tool update: cask-add

--- common/shell-functions.sh
+++ common/shell-functions.sh
@@ -5,3 +5,10 @@ brew-add() {
     brew install "$tool" || return 1
     add-tool.sh "$tool" "$@"
 }
+cask-add() {
+    local tool="$1"
+    shift
+
+    brew install "$tool" || return 1
+    add-tool.sh "$tool" --gui "$@"
+}
